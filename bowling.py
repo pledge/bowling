6d22a8e6cf72444c2c7471ce38af3e60cbb0a2d0
Martin Gilday <mgilday@imap.cc>
2012-05-16 21:45:06 +0100

    Tracking simple total without bonus

--- bowling.py
+++ bowling.py
@@ -1,20 +1,43 @@
 class Game:
 	def __init__(self):
-		self.frames = []
+		self.frames = [Frame(1)]
 		self.frame = 1
 	
 	def roll(self, pins):
-		pass
+		if self.frames[-1].is_complete():
+			self.frame += 1
+			self.frames.append(Frame(self.frame))
+		
+		f = self.frames[-1]
+		f.roll(pins)
+	
+	def total(self):
+		return reduce(lambda acc, f: acc + f.total(), self.frames, 0)
 
 class Frame:
-	def __init__(self):
+	def __init__(self, frame):
+		self.frame = frame
 		self.bowls = []
 
 	def roll(self, pins):
 		self.bowls.append(pins)
 
-	def isStrike(self):
-		return len(self.bowls) == 1 and sum(self.bowls) == 10
+	def is_complete(self):
+		if (self.frame != 10):
+			return sum(self.bowls) == 10 or len(self.bowls) == 2
+		else:
+			n = len(self.bowls)
+			if n == 1:
+				return False
+			if n == 3 :
+				return True
+			if n == 2 and self.bowls[0] == 10:
+				return False
+			if n == 2 and sum(self.bowls) == 10:
+				return False
+			# error here as unexpected?
+			return True
 
-	def isSpare(self):
-		return len(self.bowls) == 2 and sum(self.bowls) == 10
+	def total(self):
+		return sum(self.bowls)
+
--- bowling_test.py
+++ bowling_test.py
@@ -6,41 +6,20 @@ class GameTest(unittest.TestCase):
 	def test_correct_start_state(self):
 		g = Game()
 		
-		self.assertEqual(g.frames, [])
 		self.assertEqual(g.frame, 1)
+		self.assertEqual(g.total(), 0)
+	
+	def test_perfect_game(self):
+		g = Game()
+		for x in range(12):
+			g.roll(10)
+		self.assertEqual(g.total(), 300)
 
 class FrameTest(unittest.TestCase):
 
 	def test_correct_start_state(self):
-		f = Frame()
+		f = Frame(1)
 		self.assertEqual(f.bowls, [])
-
-	def test_is_strike(self):
-		f = Frame()
-		f.roll(10)
-		self.assertTrue(f.isStrike())
-
-		f2 = Frame()
-		f2.roll(1)
-		self.assertFalse(f2.isStrike())
-		f2.roll(2)
-		self.assertFalse(f2.isStrike())
-
-		f3 = Frame()
-		f2.roll(5)
-		f2.roll(5)
-		self.assertFalse(f3.isStrike())
-	
-	def test_is_spare(self):
-		f = Frame()
-		f.roll(5)
-		f.roll(5)
-		self.assertTrue(f.isSpare())
-
-		f2 = Frame()
-		f2.roll(5)
-		f2.roll(4)
-		self.assertFalse(f2.isSpare())
 
 
 if __name__ == '__main__':
