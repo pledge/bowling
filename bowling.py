2375a84f32056b810f0284e868f18f59ce53c01d
Martin Gilday <mgilday@imap.cc>
2012-05-16 23:15:54 +0100

    Perfect game working

--- bowling.py
+++ bowling.py
@@ -1,43 +1,77 @@
 class Game:
 	def __init__(self):
-		self.frames = [Frame(1)]
 		self.frame = 1
+		self.frames = [self._create_frame()]
 	
 	def roll(self, pins):
 		if self.frames[-1].is_complete():
 			self.frame += 1
-			self.frames.append(Frame(self.frame))
+			self.frames.append(self._create_frame())
 		
 		f = self.frames[-1]
 		f.roll(pins)
+
+	def running_total(self):
+		result = []
+
+		def flatten(acc, f):
+			acc.extend(f.bowls)
+			return acc
+
+		flat = reduce(flatten, self.frames, [])
+		pos =  -1
+
+		for i, val in enumerate(self.frames):
+			bonus = 0
+			if val.isStrike():
+				pos += 1
+				bonus = sum(flat[pos+1:pos+3])
+			elif val.isSpare():
+				pos += 2
+				bonus = sum(flat[pos+1:pos+2])
+			else:
+				pos += len(val.bowls)
+
+			prev = result[i - 1] if result else 0
+			result.append(prev + val.total() + bonus)
+		return result
 	
 	def total(self):
-		return reduce(lambda acc, f: acc + f.total(), self.frames, 0)
+		return self.running_total()[-1]
+	
+	def _create_frame(self):
+		if(self.frame == 10):
+			return Frame10()
+		return Frame()
 
 class Frame:
-	def __init__(self, frame):
-		self.frame = frame
+	def __init__(self,):
 		self.bowls = []
 
 	def roll(self, pins):
 		self.bowls.append(pins)
 
 	def is_complete(self):
-		if (self.frame != 10):
-			return sum(self.bowls) == 10 or len(self.bowls) == 2
-		else:
-			n = len(self.bowls)
-			if n == 1:
-				return False
-			if n == 3 :
-				return True
-			if n == 2 and self.bowls[0] == 10:
-				return False
-			if n == 2 and sum(self.bowls) == 10:
-				return False
-			# error here as unexpected?
-			return True
+		return sum(self.bowls) == 10 or len(self.bowls) == 2
 
 	def total(self):
 		return sum(self.bowls)
+	
+	def isStrike(self):
+		return self.is_complete() and self.bowls[0] == 10
+	
+	def isSpare(self):
+		return self.is_complete() and sum(self.bowls) == 10
 
+class Frame10(Frame):
+	def __init__(self):
+		Frame.__init__(self)
+	
+	def is_complete(self):
+		n = len(self.bowls)
+		if n == 2 and (self.bowls[0] == 10 or sum(self.bowls) == 10):
+			return False
+		return n != 1
+
+	def total(self):
+		return self.bowls[0] if self.bowls else 0
--- bowling_test.py
+++ bowling_test.py
@@ -8,6 +8,13 @@ class GameTest(unittest.TestCase):
 		
 		self.assertEqual(g.frame, 1)
 		self.assertEqual(g.total(), 0)
+
+	def test_running_total_no_bonus(self):
+		g = Game()
+		g.roll(3)
+		self.assertEqual(g.running_total(), [3])
+		g.roll(4)
+		self.assertEqual(g.running_total(), [7])
 	
 	def test_perfect_game(self):
 		g = Game()
@@ -18,7 +25,7 @@ class GameTest(unittest.TestCase):
 class FrameTest(unittest.TestCase):
 
 	def test_correct_start_state(self):
-		f = Frame(1)
+		f = Frame()
 		self.assertEqual(f.bowls, [])
 
 
