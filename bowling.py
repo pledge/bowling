ba2b00d0e77f3823705b5a6954104cab84a361ae
Martin Gilday <mgilday@imap.cc>
2012-05-16 21:15:41 +0100

    Strike and spare tests

--- bowling.py
+++ bowling.py
@@ -9,3 +9,12 @@ class Game:
 class Frame:
 	def __init__(self):
 		self.bowls = []
+
+	def roll(self, pins):
+		self.bowls.append(pins)
+
+	def isStrike(self):
+		return len(self.bowls) == 1 and sum(self.bowls) == 10
+
+	def isSpare(self):
+		return len(self.bowls) == 2 and sum(self.bowls) == 10
--- bowling_test.py
+++ bowling_test.py
@@ -13,8 +13,35 @@ class FrameTest(unittest.TestCase):
 
 	def test_correct_start_state(self):
 		f = Frame()
-
 		self.assertEqual(f.bowls, [])
+
+	def test_is_strike(self):
+		f = Frame()
+		f.roll(10)
+		self.assertTrue(f.isStrike())
+
+		f2 = Frame()
+		f2.roll(1)
+		self.assertFalse(f2.isStrike())
+		f2.roll(2)
+		self.assertFalse(f2.isStrike())
+
+		f3 = Frame()
+		f2.roll(5)
+		f2.roll(5)
+		self.assertFalse(f3.isStrike())
+	
+	def test_is_spare(self):
+		f = Frame()
+		f.roll(5)
+		f.roll(5)
+		self.assertTrue(f.isSpare())
+
+		f2 = Frame()
+		f2.roll(5)
+		f2.roll(4)
+		self.assertFalse(f2.isSpare())
+
 
 if __name__ == '__main__':
 	unittest.main()
